cf94be07e08196041e700af84f2207638db43e03
Roman Podgaetsky <romanp@MacBook-Air-sl-Roman.local>
2026-05-25 18:27:56 +0300

    feat: wire MobileNav into AppLayout with mobile padding

--- client/src/components/layout/AppLayout.tsx
+++ client/src/components/layout/AppLayout.tsx
@@ -1,13 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
+import MobileNav from './MobileNav';
 
 export default function AppLayout() {
   return (
     <div className="flex min-h-screen bg-slate-900">
       <Sidebar />
-      <main className="flex-1 bg-white text-slate-900 overflow-y-auto min-h-screen">
-        <Outlet />
-      </main>
+      <div className="flex flex-col flex-1">
+        <MobileNav />
+        <main className="flex-1 bg-white text-slate-900 overflow-y-auto min-h-screen pt-[88px] md:pt-0">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
